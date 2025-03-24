224a5f81d84cad634efa8fe83fcbd71dfd0ad688
ITGuns <it3.digitalconnections@gmail.com>
2025-03-24 14:24:09 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Direct connection URL from your Supabase dashboard
 const supabaseUrl = 'https://siyulhaedbbhvwggttkl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeXVsaGFlZGJiaHZ3Z2d0dGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3OTE5MTIsImV4cCI6MjA1ODM2NzkxMn0.CgOAjYqeZLnJIZOVx7gVkoHQjfiuu-Ai6IKhlJ95ldE'
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export { supabase }
